bf06d469d5ad108e64b2b38b95e4f008a1287fa3
MJavadHzr <hezarej@gmail.com>
2025-01-03 23:05:58 +0330

    add flask app

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import argparse
+from flask import Flask, jsonify
+from database import DataBase
+from sim_founder import SimFounder
+
+app = Flask(__name__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', type=int, default=8080)
+parser.add_argument('--db-name', type=str, required=True)
+parser.add_argument('--user', type=str, required=True)
+parser.add_argument('--password', type=str, required=True)
+parser.add_argument('--model', type=str, default='all-MiniLM-L6-v2')
+parser.add_argument('--threshold', type=float, default=0.5)
+args = parser.parse_args()
+
+
+@app.route("/vulnerabilities", methods=['GET'])
+def vulnerabilities():
+    result = sim_founder.get_sims()
+    # return json.dumps(result, indent=4)
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    db = DataBase(
+        args.db_name,
+        args.user,
+        args.password
+    )
+    sim_founder = SimFounder(db, args.model, args.threshold)
+
+    app.run(port=args.port, debug=False)
