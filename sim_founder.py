5d7803f3d45d28e93cf0bfe2c9e7094d26196b06
MJavadHzr <hezarej@gmail.com>
2025-01-03 23:06:27 +0330

    sim founder v1.1

--- sim_founder.py
+++ sim_founder.py
@@ -1,6 +1,5 @@
-from sql_queries import *
-from itertools import product
 import torch
+from sql_queries import *
 from sentence_transformers import SentenceTransformer
 
 
@@ -12,21 +11,32 @@ class SimFounder:
         self.groups = dict()  # ids -> group-num string
 
     def get_sims(self):
+        # group records with cve
         not_null_cve = self.db.query(NOT_NULL_CVE_QUERY)
-        self._update_groups(not_null_cve)
+        self._group_cve(not_null_cve)
+        
+        # group records with null cve
         null_cve = self.db.query(NULL_CVE_QUERY)
-        null_cve = self._find_null_cve_groups(null_cve)
-        return not_null_cve + null_cve
+        self._group_null_cve(null_cve)
 
-    def _update_groups(self, records):
+        return self._group_to_dict()
+
+    def _group_to_dict(self):
+        output_dict = self.db.query(SELECT_QUERY.format(columns='*'), return_dict=True)
+        for o in output_dict:
+            o['tag'] = self.groups[o['id']]
+        return output_dict
+
+
+    def _group_cve(self, records):
         ids = [r[0] for r in records]
         groups = [r[1] for r in records]
         self.groups = dict(zip(ids, groups))
 
-    def _find_null_cve_groups(self, null_cve):
+    def _group_null_cve(self, null_cve):
         null_ids = [r[0] for r in null_cve]
 
-        all_desc = self.db.query(SELECT_QUERY)
+        all_desc = self.db.query(SELECT_QUERY.format(columns='id, title, description'))
         idx_to_ids = [r[0] for r in all_desc]
         ids_to_idx = dict(zip(idx_to_ids, range(len(idx_to_ids))))
 
@@ -35,7 +45,7 @@ class SimFounder:
         most_sim_idx = indices[:, 1]
         most_sim_val = values[:, 1]
 
-        self._team_up(null_ids, most_sim_idx, most_sim_val)
+        self._team_up(null_ids, most_sim_idx, most_sim_val, idx_to_ids)
         self._assign_remaining([id for id in null_ids if id not in self.groups.keys()])
 
     def _calculate_sims(self, null_ids, all_desc, ids_to_idx):
@@ -56,7 +66,7 @@ class SimFounder:
                 elif target_ids in self.groups.keys():
                     self.groups[src_ids] = self.groups[target_ids]
                 else:
-                    new_group = f"group_{len(set(self.groups.values())+1)}"
+                    new_group = f"group_{len(set(self.groups.values()))+1}"
                     self.groups[src_ids] = new_group
                     self.groups[target_ids] = new_group
 
@@ -64,5 +74,5 @@ class SimFounder:
         if len(not_assigned_null_ids) == 0:
             return
         for id in not_assigned_null_ids:
-            new_group = f"group_{len(set(self.groups.values())+1)}"
+            new_group = f"group_{len(set(self.groups.values()))+1}"
             self.groups[id] = new_group
