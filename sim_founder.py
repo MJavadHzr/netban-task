66d70a10ae77fb2884b7515e2f87995fd81c813d
MJavadHzr <hezarej@gmail.com>
2025-01-04 12:34:11 +0330

    update matching strategy

--- sim_founder.py
+++ sim_founder.py
@@ -14,7 +14,7 @@ class SimFounder:
         # group records with cve
         not_null_cve = self.db.query(NOT_NULL_CVE_QUERY)
         self._group_cve(not_null_cve)
-        
+
         # group records with null cve
         null_cve = self.db.query(NULL_CVE_QUERY)
         self._group_null_cve(null_cve)
@@ -27,7 +27,6 @@ class SimFounder:
             o['tag'] = self.groups[o['id']]
         return output_dict
 
-
     def _group_cve(self, records):
         ids = [r[0] for r in records]
         groups = [r[1] for r in records]
@@ -36,39 +35,39 @@ class SimFounder:
     def _group_null_cve(self, null_cve):
         null_ids = [r[0] for r in null_cve]
 
-        all_desc = self.db.query(SELECT_QUERY.format(columns='id, title, description'))
+        all_desc = self.db.query(SELECT_QUERY.format(columns='id, title, description, endpoint'))
         idx_to_ids = [r[0] for r in all_desc]
         ids_to_idx = dict(zip(idx_to_ids, range(len(idx_to_ids))))
 
         sims = self._calculate_sims(null_ids, all_desc, ids_to_idx)
-        values, indices = torch.topk(sims, k=2)     # most similar is the record itself
-        most_sim_idx = indices[:, 1]
-        most_sim_val = values[:, 1]
-
-        self._team_up(null_ids, most_sim_idx, most_sim_val, idx_to_ids)
-        self._assign_remaining([id for id in null_ids if id not in self.groups.keys()])
+        self._team_up(null_ids, sims, idx_to_ids)
+        self._assign_remaining(
+            [id for id in null_ids if id not in self.groups.keys()])
 
     def _calculate_sims(self, null_ids, all_desc, ids_to_idx):
-        all_desc_str = [f"Title: {r[1]}\nDescription: {r[2]}" for r in all_desc]
+        all_desc_str = [f"Title: {r[1]}\nDescription: {r[2]}\nEnd-Point: {r[3]}" for r in all_desc]
         encodes = self.model.encode(all_desc_str)
         sims = self.model.similarity(encodes, encodes)
         sims = sims[[ids_to_idx[id] for id in null_ids], :]
         return sims
 
-    def _team_up(self, null_ids, most_sim_idx, most_sim_val, idx_to_ids):
-        for i, v in enumerate(most_sim_val):
-            if v >= self.threshold:
-                src_ids = null_ids[i]
-                target_ids = idx_to_ids[most_sim_idx[i]]
-
-                if src_ids in self.groups.keys():
-                    self.groups[target_ids] = self.groups[src_ids]
-                elif target_ids in self.groups.keys():
-                    self.groups[src_ids] = self.groups[target_ids]
-                else:
-                    new_group = f"group_{len(set(self.groups.values()))+1}"
-                    self.groups[src_ids] = new_group
-                    self.groups[target_ids] = new_group
+    def _team_up(self, null_ids, sims, idx_to_ids):
+        for i, row in enumerate(sims):
+            src_id = null_ids[i]
+            found_match = None
+            for j, value in enumerate(row):
+                if value >= self.threshold:
+                    target_id = idx_to_ids[j]
+                    if src_id == target_id:
+                        continue
+                    if target_id in self.groups.keys():
+                        found_match = target_id
+                        break
+            if found_match is None:
+                new_group = f"group_{len(set(self.groups.values())) + 1}"
+                self.groups[src_id] = new_group
+            else:
+                self.groups[src_id] = self.groups[target_id]
 
     def _assign_remaining(self, not_assigned_null_ids):
         if len(not_assigned_null_ids) == 0:
